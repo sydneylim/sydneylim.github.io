44394c840baa61df08e596113a19f94989a49906
Sydney Lim <sydneylim@ucsb.edu>
2025-01-09 21:51:21 -0800

    fix bugs for jan 2025 updates

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,6 +1,6 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+// import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-const Age = () => {
-  const [age, setAge] = useState();
+// const Age = () => {
+//   const [age, setAge] = useState();
 
-  const tick = () => {
-    const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('2000-08-29T07:00:00');
-    setAge(((Date.now() - birthTime) / divisor).toFixed(11));
-  };
+//   const tick = () => {
+//     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
+//     const birthTime = new Date('2000-08-29T07:00:00');
+//     setAge(((Date.now() - birthTime) / divisor).toFixed(11));
+//   };
 
-  useEffect(() => {
-    const timer = setInterval(() => tick(), 25);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-  return <>{age}</>;
-};
+//   useEffect(() => {
+//     const timer = setInterval(() => tick(), 25);
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, []);
+//   return <>{age}</>;
+// };
 
 const data = [
   {
